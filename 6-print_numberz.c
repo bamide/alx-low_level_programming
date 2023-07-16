6348afe70916407027bd7040c5ccd0200e81a0f0
bamide <bakremayowa2019@gmail.com>
2023-07-16 07:52:08 -0700

    6-print_numberz.c

---
+++ 6-print_numberz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single-digit numbers of base 10
+ *              starting from 0, followed by a new line.
+ *              Uses putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('0');
+	putchar('\n');
+
+	putchar('1');
+	putchar('\n');
+
+	putchar('2');
+	putchar('\n');
+
+	putchar('3');
+	putchar('\n');
+
+	putchar('4');
+	putchar('\n');
+
+	putchar('5');
+	putchar('\n');
+
+	putchar('6');
+	putchar('\n');
+
+	putchar('7');
+	putchar('\n');
+
+	putchar('8');
+	putchar('\n');
+
+	putchar('9');
+	putchar('\n');
+
+	return (0);
+}
