c92c5e10dd89583792c5c42cb062fd25b9688561
bamide <bakremayowa2019@mail.com>
2023-07-22 00:45:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+/* Custom _putchar function (as it's only allowed to use twice) */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ */
+void print_alphabet(void)
+{
+    char ch;
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        _putchar(ch);
+    }
+    _putchar('\n');
+}
+
+/**
+ * main - Entry point of the program (for testing).
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+    print_alphabet();
+    return 0;
+}
