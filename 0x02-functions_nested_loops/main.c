7b20c91d519d9d845166145d12709ff80fc3c857
bamide <bakremayowa2019@mail.com>
2023-07-23 19:07:49 -0700

    main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,33 +1,12 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - Writes a character to the standard output (stdout).
- * @c: The character to be written.
+ * main - check the code.
  *
- * Return: On success, returns the character written.
- * On error, returns -1 and sets errno appropriately.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (success).
+ * Return: Always 0.
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return 0;
+    print_alphabet_x10();
+    return (0);
 }
