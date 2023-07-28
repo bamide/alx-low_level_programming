ca1004aac8238d5283527b543525d749fa2e86f7
bamide <bakremayowa2019@mail.com>
2023-07-28 03:02:36 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,39 @@
-#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
 
 /**
  * cap_string - Capitalizes all words in a string.
- * @str: The input string to capitalize.
+ * @str: The input string.
  *
- * Return: A pointer to the capitalized string.
+ * Return: Pointer to the modified string.
  */
 char *cap_string(char *str)
 {
-    int capitalize_next = 1;
+    int capitalize_next = 1; // Flag to indicate if the next character should be capitalized
 
     for (int i = 0; str[i] != '\0'; i++)
     {
-        if (isspace(str[i]) || str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' ||
-            str[i] == ')' || str[i] == '{' || str[i] == '}')
+        // Check if the current character is a separator
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+            str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' ||
+            str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' ||
+            str[i] == '{' || str[i] == '}')
         {
-            capitalize_next = 1;
+            capitalize_next = 1; // Set the flag to capitalize the next character
         }
         else if (capitalize_next)
         {
+            // If the flag is set, capitalize the current character and reset the flag
             str[i] = toupper(str[i]);
             capitalize_next = 0;
+        }
+        else
+        {
+            // If the flag is not set, convert the current character to lowercase
+            str[i] = tolower(str[i]);
         }
     }
 
     return str;
 }
+
