1b19a00cbd6fa22c7b7c8d9b5fdd9ce045972f8d
bamide <bakremayowa2019@mail.com>
2023-07-29 15:26:48 -0700

    Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * mul - Function that multiplies two integers.
@@ -11,25 +11,4 @@
 int mul(int a, int b)
 {
 	return (a * b);
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (Success).
- */
-int main(void)
-{
-	int num1, num2, result;
-
-	printf("Enter the first integer: ");
-	scanf("%d", &num1);
-
-	printf("Enter the second integer: ");
-	scanf("%d", &num2);
-
-	result = mul(num1, num2);
-	printf("The product of %d and %d is: %d\n", num1, num2, result);
-
-	return (0);
 }
