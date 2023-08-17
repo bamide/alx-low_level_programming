24f6ce9edc01bd6a5db85bc635330ea90e81fad8
bamide <bakremayowa2019@gmail.com>
2023-08-17 13:04:43 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,64 +1,32 @@
-#include <stdio.h>
-#include "function_pointers.h"
 #include "3-calc.h"
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - Entry point of the program
- * @argc: The number of command line arguments
- * @argv: An array containing the command line arguments
- *
- * Return: 0 on success, 98 for wrong argument count, 99 for invalid operator,
- *         and 100 for division/modulo by zero.
- */
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return 98;
-    }
+	int (*oprt)(int, int);
 
-    int num1 = atoi(argv[1]);
-    char operator = argv[2][0];
-    int num2 = atoi(argv[3]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    int result;
+	oprt = get_op_func(argv[2]);
 
-    switch (operator)
-    {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2 == 0)
-            {
-                printf("Error\n");
-                return 100;
-            }
-            result = num1 / num2;
-            break;
-        case '%':
-            if (num2 == 0)
-            {
-                printf("Error\n");
-                return 100;
-            }
-            result = num1 % num2;
-            break;
-        default:
-            printf("Error\n");
-            return 99;
-    }
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    printf("%d\n", result);
-    return 0;
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
