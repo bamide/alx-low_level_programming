5ad90125260f3550483368997e482b5d7f56066e
bamide <bakremayowa2019@gmail.com>
2023-08-16 18:02:45 -0700

    Update all

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - function with two arguments
+ * @a: int type
+ * @b: int type
+ *
+ * Description: add two input
+ * Return: value
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function with two arguments
+ * @a: int type
+ * @b: int type
+ *
+ * Description: subtract two input
+ * Return: value
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function with two arguments
+ * @a: int type
+ * @b: int type
+ *
+ * Description: multiply two input
+ * Return: value
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
