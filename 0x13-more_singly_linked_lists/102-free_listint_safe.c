a698dba75e6622773e1af9725b1657d3b7c9fc9d
bamide <bakremayowa2019@gmail.com>
2023-08-30 13:55:32 -0700

    update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,27 +11,49 @@
  *
  * Return: The size of the list that was freed.
  */
-typedef struct listint_t
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-    int data;
-    struct listint_t *next;
-} listint_t;
+	listint_t **newlist;
+	size_t i;
 
-size_t free_listint_safe(listint_t **h)
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+} 
+size_t free_listint_safe(listint_t **head)
 {
-    size_t size = 0;
-    listint_t *current = *h;
-    listint_t *temp;
+	size_t i, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-    while (current != NULL)
-    {
-        temp = current;
-        current = current->next;
-        free(temp);
-        size++;
-    }
-
-    *h = NULL;
-    return size;
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (num);
 }
 
