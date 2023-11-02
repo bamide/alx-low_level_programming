cd30ef52b710754a6c44f3afc944eee74b692a3d
bamide <bakremayowa2019@gmail.com>
2023-11-02 13:02:30 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,12 @@
-#include <unistd.h>
+#include "main.h"
 
-int _putchar(char c);
 
-// Function to print 10 times the numbers from 0 to 14
+/**
+ * more_numbers - nmbr fct
+ * Bwave ICT file
+ * Return: no return
+ */
+
 void more_numbers(void)
 {
     int i, j;
@@ -25,15 +29,3 @@ void more_numbers(void)
     }
 }
 
-// Custom _putchar function (you can use your own implementation)
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
-// Test the function
-int main(void)
-{
-    more_numbers();
-    return 0;
-}
