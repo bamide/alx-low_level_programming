878c9cdb55474c7a3583ad9ec536dd32383350e7
bamide <bakremayowa2019@gmail.com>
2023-11-23 12:52:45 -0800

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,6 +6,7 @@
  * multiply_numbers - Multiplies two integers
  * @num1: First integer
  * @num2: Second integer
+ *
  * Return: Result of the multiplication
  */
 int multiply_numbers(int num1, int num2)
@@ -16,12 +17,14 @@ int multiply_numbers(int num1, int num2)
 /**
  * is_valid_input - Checks if the input is a valid positive integer
  * @arg: Input string to check
+ *
  * Return: 1 if valid, 0 otherwise
  */
 int is_valid_input(const char *arg)
 {
     while (*arg)
     {
+        /* Check if the character is a digit */
         if (!isdigit(*arg))
         {
             return 0;
@@ -35,27 +38,33 @@ int is_valid_input(const char *arg)
  * main - Entry point of the program
  * @argc: Number of command-line arguments
  * @argv: Array of command-line argument strings
+ *
  * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
+    /* Check if the number of arguments is correct */
     if (argc != 3)
     {
         printf("Error\n");
         return 98;
     }
 
+    /* Extract command-line arguments */
     char *num1 = argv[1];
     char *num2 = argv[2];
 
+    /* Check if arguments are valid positive integers */
     if (!(is_valid_input(num1) && is_valid_input(num2)))
     {
         printf("Error\n");
         return 98;
     }
 
+    /* Perform multiplication and print the result */
     int result = multiply_numbers(atoi(num1), atoi(num2));
     printf("%d\n", result);
 
     return 0;
 }
+
